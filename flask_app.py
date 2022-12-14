83b8c2db38b367a601cdbacefb56beb767bfc871
unknown <lichen.liu@realtek.com>
2022-12-14 12:57:55 +0800

    test

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,46 @@
+from flask import Flask, request, render_template, redirect, url_for
 
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, render_template
 
 app = Flask(__name__)
+hw_status = 'Doing HW'
+hw_status = 'Terminated'
+hw_status = 'Getting HW'
+
+
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("todo.html")
+    
+@app.route('/hpma_controller')
+def hpma_controller():
+    return render_template("hpma_controller.html", status=hw_status)
+    
+@app.route('/set_cur_hw_got', methods=['GET', 'POST'])
+def set_cur_hw_got():
+    global hw_status
+    hw_status = 'Doing HW'
+    return redirect(url_for('hpma_controller', status=hw_status))
+    
+@app.route('/set_cur_hw_done', methods=['GET', 'POST'])
+def set_cur_hw_done():
+    global hw_status
+    hw_status = 'Getting HW'
+    return redirect(url_for('hpma_controller', status=hw_status))
+    
+@app.route('/set_all_hw_done', methods=['GET', 'POST'])
+def set_all_hw_done():
+    global hw_status
+    hw_status = 'Terminated'
+    return redirect(url_for('hpma_controller', status=hw_status))
+    
+    
+@app.route('/get_hw_status', methods=['GET', 'POST'])
+def get_hw_status():
+    global hw_status
+    return hw_status
 
 if __name__ == '__main__':
+    hw_status = 'Getting HW'
     app.run('0.0.0.0', port=5000, debug=True)
+    
--- static/todo.py
+++ static/todo.py
@@ -1,6 +1,19 @@
 from datetime import datetime as dt
 
-from utils import add_class, remove_class
+def format_date(dt_, fmt="%m/%d/%Y, %H:%M:%S"):
+    return f"{dt_:{fmt}}"
+
+
+def now(fmt="%m/%d/%Y, %H:%M:%S"):
+    return format_date(dt.now(), fmt)
+
+
+def remove_class(element, class_name):
+    element.element.classList.remove(class_name)
+
+
+def add_class(element, class_name):
+    element.element.classList.add(class_name)
 
 tasks = []
 
