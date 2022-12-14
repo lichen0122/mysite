a40b68dbcae6daf214c58b8c4ce0bee9c2872e51
unknown <lichen.liu@realtek.com>
2022-12-14 13:26:03 +0800

    test

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -6,35 +7,41 @@ hw_status = 'Doing HW'
 hw_status = 'Terminated'
 hw_status = 'Getting HW'
 
-
+    
+    
 
 @app.route('/')
 def hello_world():
-    return render_template("todo.html")
+    # return render_template("todo.html")
+    return 'HI'
     
+
 @app.route('/hpma_controller')
 def hpma_controller():
     return render_template("hpma_controller.html", status=hw_status)
     
+
 @app.route('/set_cur_hw_got', methods=['GET', 'POST'])
 def set_cur_hw_got():
     global hw_status
     hw_status = 'Doing HW'
     return redirect(url_for('hpma_controller', status=hw_status))
     
+
 @app.route('/set_cur_hw_done', methods=['GET', 'POST'])
 def set_cur_hw_done():
     global hw_status
     hw_status = 'Getting HW'
     return redirect(url_for('hpma_controller', status=hw_status))
     
+
 @app.route('/set_all_hw_done', methods=['GET', 'POST'])
 def set_all_hw_done():
     global hw_status
     hw_status = 'Terminated'
     return redirect(url_for('hpma_controller', status=hw_status))
     
-    
+
 @app.route('/get_hw_status', methods=['GET', 'POST'])
 def get_hw_status():
     global hw_status
