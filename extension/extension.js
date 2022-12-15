3acbfc62490a14d0322b3099ddb9de5de1a75298
unknown <lichen.liu@realtek.com>
2022-12-15 14:41:56 +0800

    test

--- extension/extension.js
+++ extension/extension.js
@@ -7,21 +7,48 @@ chrome.runtime.onMessageExternal.addListener(
       });
       return true;
     }
-    const sources = message.sources;
-    const tab = sender.tab;
-    chrome.desktopCapture.chooseDesktopMedia(sources, tab, streamId => {
-      if (!streamId) {
-        sendResponse({
-          type: 'error',
-          message: 'Failed to get stream ID'
-        });
-      } else {
-        sendResponse({
-          type: 'success',
-          streamId: streamId
-        });
-      }
-    });
-    return true;
+	if (message.type == 'start_record'){
+		const sources = message.sources;
+		const tab = sender.tab;
+		chrome.desktopCapture.chooseDesktopMedia(sources, tab, streamId => {
+		  if (!streamId) {
+			sendResponse({
+			  type: 'error',
+			  message: 'Failed to get stream ID'
+			});
+		  } else {
+			sendResponse({
+			  type: 'success',
+			  streamId: streamId
+			});
+		  }
+		});
+		return true;
+	}
+  }
+);
+
+
+chrome.runtime.onMessage.addListener(
+  (message, sender, sendResponse) => {
+
+	if (message.type == 'start_record'){
+		const sources = message.sources;
+		const tab = sender.tab;
+		chrome.desktopCapture.chooseDesktopMedia(sources, tab, streamId => {
+		  if (!streamId) {
+			sendResponse({
+			  type: 'error',
+			  message: 'Failed to get stream ID'
+			});
+		  } else {
+			sendResponse({
+			  type: 'success',
+			  streamId: streamId
+			});
+		  }
+		});
+		return true;
+	}
   }
 );
