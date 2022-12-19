0f4cb9edf27959848fdc776b06f62b32ca089bc6
lichen0122 <qwetyuvbn@gmail.com>
2022-12-19 22:17:40 +0800

    test

--- flask_app.py
+++ flask_app.py
@@ -6,6 +6,9 @@ hw_status = 'Doing HW'
 hw_status = 'Terminated'
 hw_status = 'Getting HW'
 
+script_status = 'Doing'
+script_status = 'Terminated'
+
 
 @app.route('/screen', methods=['GET', 'POST'])
 def screen():
@@ -20,28 +23,28 @@ def hello_world():
 
 @app.route('/hpma_controller', methods=['GET', 'POST'])
 def hpma_controller():
-    return render_template("hpma_controller.html", status=hw_status)
+    return render_template("hpma_controller.html", status=hw_status, script_status=script_status)
     
 
-@app.route('/set_cur_hw_got', methods=['GET', 'POST'])
-def set_cur_hw_got():
-    global hw_status
-    hw_status = 'Doing HW'
-    return redirect(url_for('hpma_controller', status=hw_status))
+# @app.route('/set_cur_hw_got', methods=['GET', 'POST'])
+# def set_cur_hw_got():
+    # global hw_status
+    # hw_status = 'Doing HW'
+    # return redirect(url_for('hpma_controller', status=hw_status, script_status=script_status))
     
 
-@app.route('/set_cur_hw_done', methods=['GET', 'POST'])
-def set_cur_hw_done():
-    global hw_status
-    hw_status = 'Getting HW'
-    return redirect(url_for('hpma_controller', status=hw_status))
+# @app.route('/set_cur_hw_done', methods=['GET', 'POST'])
+# def set_cur_hw_done():
+    # global hw_status
+    # hw_status = 'Getting HW'
+    # return redirect(url_for('hpma_controller', status=hw_status, script_status=script_status))
     
 
-@app.route('/set_all_hw_done', methods=['GET', 'POST'])
-def set_all_hw_done():
-    global hw_status
-    hw_status = 'Terminated'
-    return redirect(url_for('hpma_controller', status=hw_status))
+# @app.route('/set_all_hw_done', methods=['GET', 'POST'])
+# def set_all_hw_done():
+    # global hw_status
+    # hw_status = 'Terminated'
+    # return redirect(url_for('hpma_controller', status=hw_status, script_status=script_status))
     
 
 @app.route('/get_hw_status', methods=['GET', 'POST'])
@@ -49,12 +52,26 @@ def get_hw_status():
     global hw_status
     return hw_status
     
+@app.route('/get_script_status', methods=['GET', 'POST'])
+def get_script_status():
+    global script_status
+    return script_status
+    
 @app.route('/json_handler', methods=['POST'])
 def json_handler(): 
     global hw_status
     request_json = request.get_json()
-    print(request_json)
-    hw_status = request_json['hw_status']
+    
+    try:
+        hw_status = request_json['hw_status']
+    except:
+        ''
+        
+    try:
+        script_status = request_json['script_status']
+    except:
+        'script_status'
+    
     
     return 'OK'
     
